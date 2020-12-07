17f67c05cebce8cd45a7f9151005f10b4f93f6f8
AmandaHassoun <amanda.hassoun@mail.utoronto.ca>
2020-12-07 14:38:44 -0500

    clean up the code l8ter

---
+++ 2020/day-2/part1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	file, err := os.Open("/Users/ahassoun/Documents/adventofcode/2020/day-2/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	validPasswords := 0
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ":")
+		policy := strings.TrimSpace(line[0])
+		splitPolicy := strings.Fields(policy)
+		validCounts := strings.Split(splitPolicy[0], "-")
+		character := splitPolicy[1]
+		password:= strings.TrimSpace(line[1])
+		actualCount := strings.Count(password, character)
+		minCount, err := strconv.Atoi(validCounts[0])
+		check(err)
+		maxCount, err := strconv.Atoi(validCounts[1])
+		check(err)
+		if actualCount >= minCount && actualCount <= maxCount {
+			validPasswords += 1
+		}
+	}
+	fmt.Printf("Total number of valid passwords: %d \n", validPasswords)
+}
