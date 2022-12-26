715a0b756eaf00adc68832acef404c282df8ae32
Amanda Hassoun <ahassoun@imgix.com>
2022-12-26 17:25:17 -0500

    day4 part1 done

---
+++ 2022/day4/day4.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	// PART 1
+	containedRangesCount := getFullyContainedRangeCount("input.txt")
+	fmt.Printf("(Part 1) Total count of assignment pairs where one range fully contains the other: %d \n \n", containedRangesCount)
+
+	// PART 2
+	//fmt.Printf("(Part 2) Total score: %d \n \n", totalScorePart2)
+}
+
+func getFullyContainedRangeCount(filename string) (int) {
+    count := 0
+	f, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatalf("Failed to open input file!!")
+	}
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+	    strategy := strings.Split(scanner.Text(), ",")
+	    contained := checkRanges(strategy[0], strategy[1])
+        if contained {
+            count++
+        }
+	}
+
+	f.Close()
+
+	return count
+}
+
+func checkRanges(range1 string, range2 string) (bool) {
+    var numericInterval1, numericInterval2 [2]int
+    interval1 := strings.Split(range1, "-")
+    interval2 := strings.Split(range2, "-")
+
+    for i := 0; i < len(interval1); i++ {
+        numericInterval1[i], _ = strconv.Atoi(interval1[i])
+        numericInterval2[i], _ = strconv.Atoi(interval2[i])
+    }
+
+    if (numericInterval1[0] >= numericInterval2[0]) && (numericInterval1[1] <= numericInterval2[1]) {
+        return true
+    } else if (numericInterval2[0] >= numericInterval1[0]) && (numericInterval2[1] <= numericInterval1[1]) {
+        return true
+    }
+
+    return false
+}
