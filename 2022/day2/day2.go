24c4e69d2da2972c179e051eb7eb360a629b4b42
Amanda Hassoun <ahassoun@imgix.com>
2022-12-25 22:26:51 -0500

    day2part2 done

--- 2022/day2/day2.go
+++ 2022/day2/day2.go
@@ -10,12 +10,16 @@ import (
 
 func main() {
 	// PART 1
-	totalScore := getStrategyGuideScore("input.txt")
-	fmt.Printf("(Part 1) Total score: %d \n \n", totalScore)
+	totalScorePart1, totalScorePart2 := getStrategyGuideScore("input.txt")
+	fmt.Printf("(Part 1) Total score: %d \n \n", totalScorePart1)
+
+	// PART 2
+	fmt.Printf("(Part 2) Total score: %d \n \n", totalScorePart2)
 }
 
-func getStrategyGuideScore(filename string) (int) {
-    totalScore := 0
+func getStrategyGuideScore(filename string) (int, int) {
+    totalScorePart1 := 0
+    totalScorePart2 := 0
 	f, err := os.Open(filename)
 
 	if err != nil {
@@ -26,12 +30,13 @@ func getStrategyGuideScore(filename string) (int) {
 
 	for scanner.Scan() {
 	    strategy := strings.Split(scanner.Text(), " ")
-	    totalScore += getScore(strategy)
+	    totalScorePart1 += getScore(strategy)
+	    totalScorePart2 += getScorePart2(strategy)
 	}
 
 	f.Close()
 
-	return totalScore
+	return totalScorePart1, totalScorePart2
 }
 
 func getScore(round []string) (int) {
@@ -65,6 +70,44 @@ func getScore(round []string) (int) {
             score = 3 + 3
         } else {
             score = 6 + 1
+        }
+    }
+
+    return score
+}
+
+func getScorePart2(round []string) (int) {
+    // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+    // Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
+    // A for Rock, B for Paper, and C for Scissors
+    // X for Rock, Y for Paper, and Z for Scissors
+    // 1 for Rock, 2 for Paper, and 3 for Scissors)
+    // 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+    score := 0
+    if round[0] == "A" {
+        if round[1] == "Y" {
+            score = 1 + 3
+        } else if round[1] == "Z" {
+            score = 2 + 6
+        } else {
+            score = 3 + 0
+        }
+    } else if round[0] == "B" {
+        if round[1] == "Y" {
+            score = 3 + 2
+        } else if round[1] == "Z" {
+            score = 6 + 3
+        } else {
+            score = 0 + 1
+        }
+    } else {
+        if round[1] == "Y" {
+            score = 3 + 3
+        } else if round[1] == "Z" {
+            score = 6 + 1
+        } else {
+            score = 2 + 0
         }
     }
 
